75a273fdc9235206b247d4f8a5b2927b3bf3d9eb
Simon Morozov <morozzmorozov@gmail.com>
2021-05-20 03:02:04 +0700

    Fix TaskD4

--- app/src/main/java/TaskD4.java
+++ app/src/main/java/TaskD4.java
@@ -12,16 +12,33 @@ public class TaskD4 {
     @Cleanup Statement statement = connection.createStatement();
 
     statement.execute("""  
-        DROP TABLE IF EXISTS flight_price;
-        CREATE TABLE flight_price (
-          id int NOT NULL PRIMARY KEY,
-          departure_airport_code char(3) NOT NULL,
-          arrival_airport_code char(3) NOT NULL,
-          price numeric(10, 2) NOT NULL,
-          fare_conditions varchar(10) NOT NULL
-        );
-        """
+      DROP TABLE IF EXISTS flight_price;
+      CREATE TABLE flight_price AS (
+      SELECT departure_airport, arrival_airport, fare_conditions, AVG(amount) amount
+        FROM ticket_flights tf
+        JOIN flights fl
+        ON tf.flight_id = fl.flight_id
+        GROUP BY fare_conditions, departure_airport, arrival_airport
+		   );
+       """
     );
+
+    statement.execute("""
+        create or replace function generate_id() returns char(6) as $$
+        declare
+        	done bool;
+        	new_id char(6);
+        begin
+                
+            done := false;
+            WHILE NOT done LOOP
+                new_id := lpad(upper(to_hex(floor(random() * 16777215) ::int)), 6, '0');
+                done := NOT exists(SELECT book_ref FROM bookings1 WHERE book_ref=new_id);
+            END LOOP;
+            RETURN new_id;
+                
+        end;
+        $$ language plpgsql;""");
 
     @Cleanup var res = statement.executeQuery("""
         SELECT departure_airport, arrival_airport, fare_conditions, AVG(amount)
