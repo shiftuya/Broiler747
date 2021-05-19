a83a8229526963940a6e8bc16a2d0858b6108b71
omarkelov <36790281+omarkelov@users.noreply.github.com>
2021-05-19 22:28:13 +0700

    Add transaction

--- app/src/main/java/db/repositories/BookingRepositoryImpl.java
+++ app/src/main/java/db/repositories/BookingRepositoryImpl.java
@@ -17,27 +17,29 @@ public class BookingRepositoryImpl implements BookingRepository {
   @SneakyThrows
   public String saveBooking(Booking booking) {
     @Cleanup Connection connection = Util.getConnection();
-    @Cleanup Statement statement = connection.createStatement();
+    try {
+      connection.setAutoCommit(false);
+      @Cleanup Statement statement = connection.createStatement();
 
-    List<Integer> prices = new ArrayList<>();
+      List<Integer> prices = new ArrayList<>();
 
-    for (int i = 0; i < booking.getPath().getFlights().size(); ++i) {
-      @Cleanup var priceResultSet = statement.executeQuery(String.format("""
+      for (int i = 0; i < booking.getPath().getFlights().size(); ++i) {
+        @Cleanup var priceResultSet = statement.executeQuery(String.format("""
               select amount from flight_price
               where departure_airport = '%s'
               and arrival_airport = '%s'
               and fare_conditions = '%s';
               """, booking.getPath().getDepartureAirports().get(i), booking.getPath().getArrivalAirports().get(i),
-          booking.getFareConditions()));
-      if (priceResultSet.next()) {
-        int price = priceResultSet.getInt("amount");
-        prices.add(price);
-      } else {
-        // exception?
+            booking.getFareConditions()));
+        if (priceResultSet.next()) {
+          int price = priceResultSet.getInt("amount");
+          prices.add(price);
+        } else {
+          // exception?
+        }
       }
-    }
 
-    @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(String.format("""
+      @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(String.format("""
         insert into bookings1 (book_ref, book_date, total_amount) values (
         	generate_id(),
         	now(),
@@ -45,20 +47,20 @@ public class BookingRepositoryImpl implements BookingRepository {
         );
         """, prices.stream().reduce(0, Integer::sum)), new String[]{"book_ref"});
 
-    preparedStatement.executeUpdate();
+      preparedStatement.executeUpdate();
 
-    @Cleanup var bookRefResultSet = preparedStatement.getGeneratedKeys();
+      @Cleanup var bookRefResultSet = preparedStatement.getGeneratedKeys();
 
-    String id;
-    if (bookRefResultSet.next()) {
-      id = bookRefResultSet.getString("book_ref");
-    } else {
-      return "-1"; // exception?
-    }
+      String id;
+      if (bookRefResultSet.next()) {
+        id = bookRefResultSet.getString("book_ref");
+      } else {
+        return "-1"; // exception?
+      }
 
-   // System.out.println(id);
+      // System.out.println(id);
 
-    @Cleanup PreparedStatement preparedStatement1 = connection.prepareStatement(String.format("""
+      @Cleanup PreparedStatement preparedStatement1 = connection.prepareStatement(String.format("""
             insert into tickets1 (ticket_no, book_ref, passenger_id, passenger_name, contact_data)
             values (
              	(select lpad(to_char(ticket_no::numeric + 1, 'FM9999999999999'), 13, '0') from tickets1
@@ -70,24 +72,24 @@ public class BookingRepositoryImpl implements BookingRepository {
             	'%s'
             );
             """, id, booking.getPassengerId(), booking.getPassengerName(),
-        new Gson().toJson(booking.getContact())
-    ), new String[]{"ticket_no"});
+          new Gson().toJson(booking.getContact())
+      ), new String[]{"ticket_no"});
 
-    preparedStatement1.executeUpdate();
+      preparedStatement1.executeUpdate();
 
-    @Cleanup var ticketResultSet = preparedStatement1.getGeneratedKeys();
+      @Cleanup var ticketResultSet = preparedStatement1.getGeneratedKeys();
 
-    String ticketId;
-    if (ticketResultSet.next()) {
-      ticketId = ticketResultSet.getString("ticket_no");
-    } else {
-      return "-1"; // exception?
-    }
+      String ticketId;
+      if (ticketResultSet.next()) {
+        ticketId = ticketResultSet.getString("ticket_no");
+      } else {
+        return "-1"; // exception?
+      }
 
-   // System.out.println(ticketId);
-    for (int i = 0; i < booking.getPath().getFlights().size(); ++i) {
+      // System.out.println(ticketId);
+      for (int i = 0; i < booking.getPath().getFlights().size(); ++i) {
 
-      statement.execute(String.format("""
+        statement.execute(String.format("""
               insert into ticket_flights1 (ticket_no, flight_id, fare_conditions, amount) values (
               	'%s',
               	%d,
@@ -95,10 +97,16 @@ public class BookingRepositoryImpl implements BookingRepository {
               	%d
               );
               """, ticketId, booking.getPath().getFlights().get(i), booking.getFareConditions(),
-          prices.get(i)));
+            prices.get(i)));
+      }
+
+      connection.commit();
+
+      return id;
+    } catch (Exception e) {
+      connection.rollback();
     }
 
-    return id;
+    return null;
   }
 }
-
