71ba8ed1d53c56f19014e198fbd24f1bfe002f20
omarkelov <36790281+omarkelov@users.noreply.github.com>
2021-05-20 00:09:47 +0700

    Add config file for password

--- app/src/main/java/server/db/util/Util.java
+++ app/src/main/java/server/db/util/Util.java
@@ -1,17 +1,24 @@
 package server.db.util;
 
+import com.google.common.io.Resources;
+import lombok.SneakyThrows;
+
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import lombok.SneakyThrows;
 
 public class Util {
   static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/demo";
   static final String DB_USER = "postgres";
-  static final String DB_PASSWORD = "admin";
+  static final String DB_PASSWORD = getPassword("db_password.txt");
+
+  @SneakyThrows
+  public static String getPassword(String path) {
+    return Resources.toString(Resources.getResource(path), StandardCharsets.UTF_8);
+  }
 
   @SneakyThrows
   public static Connection getConnection() {
     return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
   }
-
 }
