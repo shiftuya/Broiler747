f082ad11756adf1dbde6d211b3a218c981b846de
Simon Morozov <morozzmorozov@gmail.com>
2021-05-26 13:18:40 +0700

    Remove extra stuff from TaskD4

--- app/src/main/java/TaskD4.java
+++ app/src/main/java/TaskD4.java
@@ -22,34 +22,5 @@ public class TaskD4 {
 		   );
        """
     );
-
-    statement.execute("""
-        create or replace function generate_id() returns char(6) as $$
-        declare
-        	done bool;
-        	new_id char(6);
-        begin
-                
-            done := false;
-            WHILE NOT done LOOP
-                new_id := lpad(upper(to_hex(floor(random() * 16777215) ::int)), 6, '0');
-                done := NOT exists(SELECT book_ref FROM bookings1 WHERE book_ref=new_id);
-            END LOOP;
-            RETURN new_id;
-                
-        end;
-        $$ language plpgsql;""");
-
-    @Cleanup var res = statement.executeQuery("""
-        SELECT departure_airport, arrival_airport, fare_conditions, AVG(amount)
-        FROM ticket_flights tf
-        JOIN flights fl
-        ON tf.flight_id = fl.flight_id
-        GROUP BY fare_conditions, departure_airport, arrival_airport;
-        """);
-
-    while (res.next()) {
-      System.out.println(res.getInt("avg"));
-    }
   }
 }
